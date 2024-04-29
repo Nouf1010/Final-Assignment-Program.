ec1e4ed028869c757fe10ab2ac2a09641e848a80
nouf1010 <nouf112211@gmail.com>
2024-04-29 15:27:09 +0400

    Added setters and getters for the class 'Venue'.

--- Aggregation.py
+++ Aggregation.py
@@ -102,3 +102,40 @@ class Venue:
     # Method to remove a supplier
     def remove_supplier(self, supplier):
         self.__suppliers.remove(supplier)
+
+    #Setters and getters
+    def set_venueID(self, venueID):
+        self.__venueID = venueID
+
+    def get_venueID(self):
+        return self.__venueID
+
+    def set_venue_name(self, venue_name):
+        self.__venue_name = venue_name
+
+    def get_venue_name(self):
+        return self.__venue_name
+
+    def set_venue_address(self, venue_address):
+        self.__venue_address = venue_address
+
+    def get_venue_address(self):
+        return self.__venue_address
+
+    def set_venue_contact_num(self, venue_contact_num):
+        self.__venue_contact_num = venue_contact_num
+
+    def get_venue_contact_num(self):
+        return self.__venue_contact_num
+
+    def set_min_guests(self, min_guests):
+        self.__min_guests = min_guests
+
+    def get_min_guests(self):
+        return self.__min_guests
+
+    def set_max_guests(self, max_guests):
+        self.__max_guests = max_guests
+
+    def get_max_guests(self):
+        return self.__max_guests
