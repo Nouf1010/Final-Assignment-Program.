290400c3690941a67f3d8a607b268eae99604922
nouf1010 <nouf112211@gmail.com>
2024-04-29 15:43:01 +0400

    Also added the class 'Event' with its attributes and setters and getters from the aggregation file to the composition file.

--- Composition.py
+++ Composition.py
@@ -45,3 +45,58 @@ class Venue:
 
     def get_max_guests(self):
         return self.__max_guests
+
+class Event:
+    '''Class to represent an event'''
+    # Constructor
+    def __init__(self, eventID, type, date, time, duration, venue, guest_list=[]):
+        self.__eventID = eventID
+        self.__type = type
+        self.__date = date
+        self.__time = time
+        self.__duration = duration
+        self.__venue = venue  # Composition relationship because venue is an object in the event class
+        self.__guest_list = guest_list
+
+    #Setters and getters
+    def set_eventID(self, eventID):
+        self.__eventID = eventID
+
+    def get_eventID(self):
+        return self.__eventID
+
+    def set_type(self, type):
+        self.__type = type
+
+    def get_type(self):
+        return self.__type
+
+    def set_date(self, date):
+        self.__date = date
+
+    def get_date(self):
+        return self.__date
+
+    def set_time(self, time):
+        self.__time = time
+
+    def get_time(self):
+        return self.__time
+
+    def set_duration(self, duration):
+        self.__duration = duration
+
+    def get_duration(self):
+        return self.__duration
+
+    def set_venue(self, venue):
+        self.__venue = venue
+
+    def get_venue(self):
+        return self.__venue
+
+    def set_guest_list(self, guest_list):
+        self.__guest_list = guest_list
+
+    def get_guest_list(self):
+        return self.__guest_list
