d90d9608ce7917789c2f37e4a5b288ef74d33f54
nouf1010 <nouf112211@gmail.com>
2024-04-30 13:16:30 +0400

    Added the class 'Person' with its attributes and setters and getters.

--- Inheritance.py
+++ Inheritance.py
@@ -24,3 +24,25 @@ class Person:
 
     def get_emailP(self):
         return self._emailP
+
+
+class Client(Person):
+    '''Class to represent a client'''
+    #Constructor
+    def __init__(self, nameP, phone_numP, emailP, clientID, budget):
+        super().__init__(nameP, phone_numP, emailP) #inheriting from person class
+        self.__clientID = clientID
+        self.__budget = budget
+
+    #Setters and getters
+    def set_clientID(self, clientID):
+        self.__clientID = clientID
+
+    def get_clientID(self):
+        return self.__clientID
+
+    def set_budget(self, budget):
+        self.__budget = budget
+
+    def get_budget(self):
+        return self.__budget
