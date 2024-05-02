a610139444a6db9f0aab94c176aa01e1857f5db5
nouf1010 <nouf112211@gmail.com>
2024-05-03 01:08:38 +0400

    Created a function to add buttons to add, delete, modify, and display details for an employee.

--- GUI+Pickle.py
+++ GUI+Pickle.py
@@ -32,3 +32,21 @@ class Application(tk.Tk):  # Define a class 'Application' inheriting from 'tk.Tk
         self.add_supplier_window = None  # Window for adding supplier
         self.add_venue_window = None  # Window for adding venue
         self.add_client_window = None  # Window for adding client
+
+    def create_widgets(self):
+    #EMPLOYEE BUTTONS
+    # Create buttons for adding, deleting, modifying, and displaying employee details
+    add_employee_button = tk.Button(self, text="Add Employee", command=self.add_employee)  # Button to add an employee
+    add_employee_button.pack()  # Adding the button to the application window
+
+    delete_employee_button = tk.Button(self, text="Delete Employee", command=self.delete_employee)  # Button to delete an employee
+    delete_employee_button.pack()  # Adding the button to the application window
+
+    modify_employee_button = tk.Button(self, text="Modify Employee", command=self.modify_employee)  # Button to modify an employee
+    modify_employee_button.pack()  # Adding the button to the application window
+
+    display_employee_button = tk.Button(self, text="Display Employee", command=self.display_employee)  # Button to display employee details
+    display_employee_button.pack()  # Adding the button to the application window
+
+
+
